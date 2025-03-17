df59fee86312c2a18a9cccba07db06da4acbc614
Imrancodingal <moumeneamine5@gmail.com>
2025-03-17 16:37:23 +0000

    Python Operator II

--- README.md
+++
@@ -1,1 +0,0 @@
-# Python-Operator-II
---
+++ python.py
@@ -0,0 +1,7 @@
+# ask someone to enter anumber
+a=int (input("enter a number:"))
+# evn number -divide by 2
+if a%2!=0:
+ print ("Not even number.")
+else:
+ print ("Even number .")
